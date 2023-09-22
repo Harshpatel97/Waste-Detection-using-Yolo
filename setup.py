d3d3dc5ec702089aa2a68c5936ae7003b836e203
Harsh Patel <harshp4106@gmail.com>
2023-09-22 10:27:53 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Harsh Patel',
+    author_email= 'harshp4106@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
